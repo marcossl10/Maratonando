2de13e62a429c8e11328556d0a243de39f019351
marcossl10 <marcosslprado@gmail.com>
2025-12-13 15:26:56 -0300

    alfa2

--- maratonando_src/core/parsers/animesonline_parser.py
+++ maratonando_src/core/parsers/animesonline_parser.py
@@ -6,7 +6,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
-BASE_URL_MINHASERIE = "https://www.minhaserie.site/"
+BASE_URL_MINHASERIE = "https://minhaserie.stream/"
 HTTP_HEADERS_MINHASERIE = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Referer": BASE_URL_MINHASERIE,
