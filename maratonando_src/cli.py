8cc44db9bd1dd64c8570be55905501ea939b73a1
marcossl10 <marcosslprado@gmail.com>
2025-12-25 17:23:23 -0300

    beta

--- maratonando_src/cli.py
+++ maratonando_src/cli.py
@@ -11,7 +11,7 @@ import traceback
 # A chave DEVE corresponder ao valor do campo 'source' nos resultados de `perform_search`.
 PARSER_MAP = {
     'AnimeFire': parsers.AnimeFireParser(),
-    'MinhaSerie': parsers.MinhaSerieParser(),
+    # 'MinhaSerie': parsers.MinhaSerieParser(),
     # Adicione outros parsers aqui conforme necessário.
     # Ex: 'MeuNovoParser': parsers.MeuNovoParser(),
 }
--- maratonando_src/core/searcher.py
+++ maratonando_src/core/searcher.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 # A chave 'instance' deve conter uma instância da classe do parser.
 parsers_list: List[Dict[str, Any]] = [
     {'name': 'AnimeFire', 'instance': AnimeFireParser()},
-    {'name': 'MinhaSerie', 'instance': MinhaSerieParser()},
+    # {'name': 'MinhaSerie', 'instance': MinhaSerieParser()},
     # Se você adicionar um novo parser (ex: MeuNovoParser em meu_novo_parser.py),
     # importe-o acima e adicione-o aqui:
     # {'name': 'MeuNovoParser', 'instance': MeuNovoParser()},
